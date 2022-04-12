731ec82f63dc574e1b37e3cfb3fc2b017bce9d10
nstepp2001 <nstepp@ucsc.edu>
2022-04-11 22:13:47 -0700

    commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,6 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
         scene.add.existing(this);
         this.isFiring = false;
         this.moveSpeed = 2;
+        this.sfxRocket = scene.sound.add('sfx_rocket');
     }
 
     update() {
@@ -18,8 +19,9 @@ class Rocket extends Phaser.GameObjects.Sprite{
             }
         }
         //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.Firing) {
             this.isFiring = true;
+            this.sfxRocket.play();
         }
         //if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
@@ -27,8 +29,13 @@ class Rocket extends Phaser.GameObjects.Sprite{
         }
         //reset on miss
         if(this.y <= borderUISize * 3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
+    }
+
+    //reset rocket to ground
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
